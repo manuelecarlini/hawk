2e4d7d8c5e4240f2af439be7d0a48e0ace0f5167
manuelecarlini <mcarlini@suse.com>
2014-11-04 16:48:53 +0100

    bootstrap template

--- hawk/test/unit/cib_test.rb
+++
@@ -1,8 +0,0 @@
-require 'test_helper'
-
-class CibTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
-  end
-end
--- hawk/test/unit/helpers/cib_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class CibHelperTest < ActionView::TestCase
-end
